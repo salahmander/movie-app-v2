6c1ee2bf0ff5f3f80df90c61347b28e2d4275ef4
salahmander <salahabdo@hotmail.co.uk>
2021-11-13 20:45:57 +0000

    Create a new matrix filter to allow the svg to change colour while someone is hovering.

--- src/Components/AppBar/AppBar.jsx
+++ src/Components/AppBar/AppBar.jsx
@@ -10,29 +10,67 @@ import iconMovie from "../../Assets/icon-movie.svg";
 import iconStartStroke from "../../Assets/icon-star-stroke.svg";
 import iconTV from "../../Assets/icon-movie.svg";
 
-const AppBar = () => {
+//https://css-tricks.com/the-many-ways-to-change-an-svg-fill-on-hover-and-when-to-use-them/
+const SvgMatrixFilter = () => {
   return (
-    <div class="app-container">
-      <div class="app-container-animate2">
-        <div class="overlap-group">
-          <img class="icon-play" src={iconPlay} alt="icon button"/>
-          <img class="icon-play-1" src={iconPlay1} alt="icon button"/>
+    <>
+      <svg>
+        <defs>
+          <filter id="icon-filter">
+            <feColorMatrix
+              colorInterpolationFilters="sRGB"
+              type="matrix"
+              values="0.33 0   0   0   0
+              0   0.22  0   0   0
+              0   0   0.80  0   0
+              0   0   0   1   0 "
+            />
+          </filter>
+        </defs>
+      </svg>
+    </>
+  );
+};
+
+const AppBar = () => {
+  const homeButtonHandler = () => {
+    console.log("Home");
+  };
+
+  const movieButtonHandler = () => {
+    console.log("Movie");
+  };
+
+  const tvSeriesButtonHandler = () => {
+    console.log("TV series");
+  };
+
+  const favouriteButtonHandler = () => {
+    console.log("Favourites");
+  };
+  return (
+    <div className="app-container">
+      <div className="app-container-animate2">
+        <div className="overlap-group">
+          <img className="icon-play" src={iconPlay} alt="icon button" />
+          <img className="icon-play-1" src={iconPlay1} alt="icon button" />
         </div>
       </div>
-      <div class="buttons">
-        <div class="icon-button">
-          <img class="icon" src={iconHome} alt="home button" />
+      <div className="buttons">
+        <div className="icon-button" onClick={homeButtonHandler}>
+          <img className="icon" src={iconHome} alt="home button" />
         </div>
-        <div class="icon-button-1">
-          <img class="icon" src={iconMovie} alt="movie button" />
+        <div className="icon-button-1" onClick={movieButtonHandler}>
+          <img className="icon" src={iconMovie} alt="movie button" />
         </div>
-        <div class="icon-button-2">
-          <img class="icon" src={iconTV} alt="tv series button"/>
+        <div className="icon-button-2" onClick={tvSeriesButtonHandler}>
+          <img className="icon" src={iconTV} alt="tv series button" />
         </div>
-        <div class="icon-button-3">
-          <img class="icon" src={iconStartStroke} alt="favourite button"/>
+        <div className="icon-button-3" onClick={favouriteButtonHandler}>
+          <img className="icon" src={iconStartStroke} alt="favourite button" />
         </div>
       </div>
+      <SvgMatrixFilter />
     </div>
   );
 };
