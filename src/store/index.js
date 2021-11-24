77f85c7451371462d92d884d00ff2b984b17802c
salahmander <salahabdo@hotmail.co.uk>
2021-11-24 21:19:39 +0000

    Add both now playing movies and tv series on air reducing fucnction into a single reducing function.

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,12 @@
+/**
+ * Keep the imports and combined reducers in alphabetical order, to maintain easy readability.
+ */
 import { combineReducers } from "redux";
 
-export default combineReducers({});
+import nowPlayingMovieReducer from "./reducers/movies/nowPlayingMoviesReducer/nowPlayingMoviesReducer";
+import tvSeriesOnAirReducer from "./reducers/tvSeries/tvSeriesOnAirReducer";
+
+export default combineReducers({
+  nowPlayingMovies: nowPlayingMovieReducer,
+  tvSeriesOnAir: tvSeriesOnAirReducer,
+});
