8c1928767773189d0048e3d74c14d1d1bd26f9c2
salahmander <salahabdo@hotmail.co.uk>
2021-11-30 16:22:54 +0000

    Refactor the carousel slider component.

--- src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
+++ src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
@@ -8,26 +8,26 @@ import iconStarFill from "../../../assets/icon-star-fill.svg";
 import tag from "../../../assets/tag.svg"
 import button from "../../../assets/button.svg"
 
-const CarouselSlider = ({name, description, image, obj, index, slideIndex}) => {
+const CarouselSlider = ({title, description, image, index, slideIndex}) => {
     return (
-        <div key={obj.id} className={slideIndex === index + 1 ? "slider active-anim" : "slider"}>
+        <div className={slideIndex === index + 1 ? "slider active-anim" : "slider"}>
         <header className="carousel-slider-container">
           <img src={image} alt="movies" />
           <div className="carousel-slider-information-container">
             <img className="tag" src={tag} alt="tag"/>
-            <div class="icon-star-container">
-              <img class="icon-star-fill-1" src={iconStarFill} alt="star-1"/>
-              <img class="icon-star-fill-2" src={iconStarFill} alt="star-2"/>
-              <img class="icon-star-fill-3" src={iconStarFill} alt="star-3"/>
-              <img class="icon-star-fill-4" src={iconStarFill} alt="star-4"/>
-              <img class="icon-star-fill-5" src={iconStarFill} alt="star-5"/>
+            <div className="icon-star-container">
+              <img className="icon-star-fill-1" src={iconStarFill} alt="star-1"/>
+              <img className="icon-star-fill-2" src={iconStarFill} alt="star-2"/>
+              <img className="icon-star-fill-3" src={iconStarFill} alt="star-3"/>
+              <img className="icon-star-fill-4" src={iconStarFill} alt="star-4"/>
+              <img className="icon-star-fill-5" src={iconStarFill} alt="star-5"/>
             </div>
-            <h1 class="title desktop-h1">{name}</h1>
-            <div class="overlap-group">
-              <p class="description mobile-card-copy">
+            <h1 className="title desktop-h1">{title}</h1>
+            <div className="overlap-group">
+              <p className="description mobile-card-copy">
                 {description}
               </p>
-              <img class="view-now-button" src={button} alt="view-now"/>
+              <img className="view-now-button" src={button} alt="view-now"/>
             </div>
           </div>
         </header>
