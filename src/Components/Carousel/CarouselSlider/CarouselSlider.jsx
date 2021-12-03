d5251464709cd96e780f22828d29f8093bbadb60
salahmander <salahabdo@hotmail.co.uk>
2021-12-03 13:02:31 +0000

    Refactor structure of the carousel slider component.

--- src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
+++ src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
+import React from "react";
 
 // CSS
-import "./CarouselSlider.css"
+import "./CarouselSlider.css";
 
 // SVG
 import iconStarFill from "../../../assets/icon-star-fill.svg";
-import tag from "../../../assets/tag.svg"
-import button from "../../../assets/button.svg"
+import tag from "../../../assets/tag.svg";
 
-const CarouselSlider = ({title, description, image, index, slideIndex}) => {
-    return (
-        <div className={slideIndex === index + 1 ? "slider active-anim" : "slider"}>
-        <header className="carousel-slider-container">
-          <img src={image} alt="movies" />
-          <div className="carousel-slider-information-container">
-            <img className="tag" src={tag} alt="tag"/>
-            <div className="icon-star-container">
-              <img className="icon-star-fill-1" src={iconStarFill} alt="star-1"/>
-              <img className="icon-star-fill-2" src={iconStarFill} alt="star-2"/>
-              <img className="icon-star-fill-3" src={iconStarFill} alt="star-3"/>
-              <img className="icon-star-fill-4" src={iconStarFill} alt="star-4"/>
-              <img className="icon-star-fill-5" src={iconStarFill} alt="star-5"/>
+const CarouselSlider = ({ title, description, image, index, slideIndex }) => {
+  return (
+    <div className={slideIndex === index + 1 ? "slider active-anim" : "slider"}>
+      <header className="carousel-slider-container">
+        <img src={image} alt="movies" />
+        <div className="carousel-slider-information-container">
+          <img className="tag" src={tag} alt="tag" />
+          <div className="icon-star-container">
+            <img className="icon-star-fill-1" src={iconStarFill} alt="star-1" />
+            <img className="icon-star-fill-2" src={iconStarFill} alt="star-2" />
+            <img className="icon-star-fill-3" src={iconStarFill} alt="star-3" />
+            <img className="icon-star-fill-4" src={iconStarFill} alt="star-4" />
+            <img className="icon-star-fill-5" src={iconStarFill} alt="star-5" />
+          </div>
+          <h1 className="title desktop-h1">{title}</h1>
+          <div className="overlap-group">
+            <div className="overlap-group-description">
+              <p className="description mobile-card-copy">{description}</p>
             </div>
-            <h1 className="title desktop-h1">{title}</h1>
-            <div className="overlap-group">
-              <p className="description mobile-card-copy">
-                {description}
-              </p>
-              <img className="view-now-button" src={button} alt="view-now"/>
+            <div className="overlap-group-view">
+              <h1>Button</h1>
             </div>
           </div>
-        </header>
-      </div>
-    )
-}
+        </div>
+      </header>
+    </div>
+  );
+};
 
-export default CarouselSlider
-
-
+export default CarouselSlider;
