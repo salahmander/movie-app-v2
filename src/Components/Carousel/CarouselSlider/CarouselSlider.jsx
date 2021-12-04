63461d1d8443140339a3575df0b1471263e77bb4
salahmander <salahabdo@hotmail.co.uk>
2021-12-04 13:34:06 +0000

    Update icon star to represent the actual ratings for the movies.

--- src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
+++ src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
@@ -4,30 +4,82 @@ import React from "react";
 import "./CarouselSlider.css";
 
 // SVG
-import iconStarFill from "../../../assets/icon-star-fill.svg";
-import tag from "../../../assets/tag.svg";
+import iconStarFill from "../../../assets/icon-star-fill-blue.svg";
+import iconStarHalfFill from "../../../assets/icon-star-half-fill-blue.svg";
+import iconStarNoFill from "../../../assets/icon-star-no-fill.svg";
+import scifi from "../../../assets/science-fiction-tag.svg";
 
 // Constants
 import { CONFIGURATIONS } from "../../../util/constants/configurations/configurationConstants";
 
 const CarouselSlider = ({ movie, index, slideIndex }) => {
-  // Destructure the configuration object values to keep variable declaration shorter
+  // Destructure the configuration object values to keep variable declaration short
   const { secure_base_url, backdrop_sizes } = CONFIGURATIONS.images;
 
   const backDrop = `${secure_base_url}${backdrop_sizes[3]}${movie.backdrop_path}`;
-
+  const ratings = movie.vote_average / 2; // Needs to be halved as the vote_average goes up to 10, it has to be 5
   return (
     <div className={slideIndex === index + 1 ? "slider active-anim" : "slider"}>
       <header className="carousel-slider-container">
         <img src={backDrop} alt="movies" />
         <div className="carousel-slider-information-container">
-          <img className="tag" src={tag} alt="tag" />
+          <img className="tag" src={scifi} alt="tag" />
           <div className="icon-star-container">
-            <img className="icon-star-fill-1" src={iconStarFill} alt="star-1" />
-            <img className="icon-star-fill-2" src={iconStarFill} alt="star-2" />
-            <img className="icon-star-fill-3" src={iconStarFill} alt="star-3" />
-            <img className="icon-star-fill-4" src={iconStarFill} alt="star-4" />
-            <img className="icon-star-fill-5" src={iconStarFill} alt="star-5" />
+            <img
+              className="icon-star-fill-1"
+              src={
+                ratings >= 1
+                  ? iconStarFill
+                  : ratings >= 0.5
+                  ? iconStarHalfFill
+                  : iconStarNoFill
+              }
+              alt="star-1"
+            />
+            <img
+              className="icon-star-fill-2"
+              src={
+                ratings >= 2
+                  ? iconStarFill
+                  : ratings >= 1.5
+                  ? iconStarHalfFill
+                  : iconStarNoFill
+              }
+              alt="star-2"
+            />
+            <img
+              className="icon-star-fill-3"
+              src={
+                ratings >= 3
+                  ? iconStarFill
+                  : ratings >= 2.5
+                  ? iconStarHalfFill
+                  : iconStarNoFill
+              }
+              alt="star-3"
+            />
+            <img
+              className="icon-star-fill-4"
+              src={
+                ratings >= 4
+                  ? iconStarFill
+                  : ratings >= 3.5
+                  ? iconStarHalfFill
+                  : iconStarNoFill
+              }
+              alt="star-4"
+            />
+            <img
+              className="icon-star-fill-5"
+              src={
+                ratings >= 5
+                  ? iconStarFill
+                  : ratings >= 4.5
+                  ? iconStarHalfFill
+                  : iconStarNoFill
+              }
+              alt="star-5"
+            />
           </div>
           <h1 className="title desktop-h1">{movie.title}</h1>
           <div className="overlap-group">
