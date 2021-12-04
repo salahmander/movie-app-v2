ddefd065d9adb4395d9d84679873e87a21889b0d
salahmander <salahabdo@hotmail.co.uk>
2021-12-04 14:02:30 +0000

    Add an overaly to the background image.

--- src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
+++ src/Components/Carousel/CarouselSlider/CarouselSlider.jsx
@@ -22,72 +22,74 @@ const CarouselSlider = ({ movie, index, slideIndex }) => {
     <div className={slideIndex === index + 1 ? "slider active-anim" : "slider"}>
       <header className="carousel-slider-container">
         <img src={backDrop} alt="movies" />
-        <div className="carousel-slider-information-container">
-          <img className="tag" src={scifi} alt="tag" />
-          <div className="icon-star-container">
-            <img
-              className="icon-star-fill-1"
-              src={
-                ratings >= 1
-                  ? iconStarFill
-                  : ratings >= 0.5
-                  ? iconStarHalfFill
-                  : iconStarNoFill
-              }
-              alt="star-1"
-            />
-            <img
-              className="icon-star-fill-2"
-              src={
-                ratings >= 2
-                  ? iconStarFill
-                  : ratings >= 1.5
-                  ? iconStarHalfFill
-                  : iconStarNoFill
-              }
-              alt="star-2"
-            />
-            <img
-              className="icon-star-fill-3"
-              src={
-                ratings >= 3
-                  ? iconStarFill
-                  : ratings >= 2.5
-                  ? iconStarHalfFill
-                  : iconStarNoFill
-              }
-              alt="star-3"
-            />
-            <img
-              className="icon-star-fill-4"
-              src={
-                ratings >= 4
-                  ? iconStarFill
-                  : ratings >= 3.5
-                  ? iconStarHalfFill
-                  : iconStarNoFill
-              }
-              alt="star-4"
-            />
-            <img
-              className="icon-star-fill-5"
-              src={
-                ratings >= 5
-                  ? iconStarFill
-                  : ratings >= 4.5
-                  ? iconStarHalfFill
-                  : iconStarNoFill
-              }
-              alt="star-5"
-            />
-          </div>
-          <h1 className="title desktop-h1">{movie.title}</h1>
-          <div className="overlap-group">
-            <div className="overlap-group-description">
-              <p className="description mobile-card-copy">{movie.overview}</p>
+        <div className="carousel-slider-overlay">
+          <div className="carousel-slider-information-container">
+            <img className="tag" src={scifi} alt="tag" />
+            <div className="icon-star-container">
+              <img
+                className="icon-star-fill-1"
+                src={
+                  ratings >= 1
+                    ? iconStarFill
+                    : ratings >= 0.5
+                    ? iconStarHalfFill
+                    : iconStarNoFill
+                }
+                alt="star-1"
+              />
+              <img
+                className="icon-star-fill-2"
+                src={
+                  ratings >= 2
+                    ? iconStarFill
+                    : ratings >= 1.5
+                    ? iconStarHalfFill
+                    : iconStarNoFill
+                }
+                alt="star-2"
+              />
+              <img
+                className="icon-star-fill-3"
+                src={
+                  ratings >= 3
+                    ? iconStarFill
+                    : ratings >= 2.5
+                    ? iconStarHalfFill
+                    : iconStarNoFill
+                }
+                alt="star-3"
+              />
+              <img
+                className="icon-star-fill-4"
+                src={
+                  ratings >= 4
+                    ? iconStarFill
+                    : ratings >= 3.5
+                    ? iconStarHalfFill
+                    : iconStarNoFill
+                }
+                alt="star-4"
+              />
+              <img
+                className="icon-star-fill-5"
+                src={
+                  ratings >= 5
+                    ? iconStarFill
+                    : ratings >= 4.5
+                    ? iconStarHalfFill
+                    : iconStarNoFill
+                }
+                alt="star-5"
+              />
             </div>
-            <div className="overlap-group-view">
-              <h1>Button</h1>
+            <h1 className="title desktop-h1">{movie.title}</h1>
+            <div className="overlap-group">
+              <div className="overlap-group-description">
+                <p className="description mobile-card-copy">{movie.overview}</p>
+              </div>
+              <div className="overlap-group-view">
+                <h1>Button</h1>
+              </div>
             </div>
           </div>
         </div>
